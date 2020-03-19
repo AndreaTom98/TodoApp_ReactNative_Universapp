25394d8be30632f942121a2456e63b035129ae3e
AndreaTomasello <andrea.tomasello1498@gmail.com>
2020-03-19 18:54:31 +0100

    sezione 2 parte 5

--- App.js
+++ App.js
@@ -1,31 +1,34 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
     <View>
-      <View style={{padding: 50, marginTop: 50, flexDirection: 'row', alignItems: 'center'}}>
+      <View style={styles.InputContainer}>
         {/* Input */}
-        <TextInput placeholder="scrivi todo" style={styles.Input} />
+        <TextInput placeholder="scrivi todo" style={[styles.Input, {borderRadius: '5%'}]} />
         <Button title="invia" onPress={() => {}} />
       </View>
       <View>
-        <Text>
-
-        </Text>
+        <Text></Text>
         {/* Output */}
       </View>
-
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   Input: {
-    width: '80%',
+    width: "80%",
     borderWidth: 1,
     borderRadius: 5,
     paddingVertical: 5,
-    paddingLeft: 3,
+    paddingLeft: 3
+  },
+  InputContainer: {
+    padding: 50,
+    marginTop: 50,
+    flexDirection: "row",
+    alignItems: "center"
   }
 });
