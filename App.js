8eb2d25320862c05b57ba8f65593def966bd0e79
AndreaTomasello <andrea.tomasello1498@gmail.com>
2020-03-19 19:23:58 +0100

    sezione 2 parte 6

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
 import React from "react";
 import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
-export default function App() {
-  return (
-    <View>
-      <View style={styles.InputContainer}>
-        {/* Input */}
-        <TextInput placeholder="scrivi todo" style={[styles.Input, {borderRadius: '5%'}]} />
-        <Button title="invia" onPress={() => {}} />
-      </View>
+export default class App extends React.Component {
+  state = {
+    value: '',
+    todoList: []
+  }
+  onChangeTextHandler = (text) => {
+    this.setState({value: text})
+  }
+  render() {
+    return (
       <View>
-        <Text></Text>
-        {/* Output */}
+        <View style={styles.InputContainer}>
+          {/* Input */}
+          <TextInput value={this.state.value} onChangeText={this.onChangeTextHandler} placeholder="scrivi todo" style={[styles.Input]} />
+          <Button title="invia" onPress={() => {}} />
+        </View>
+        <View>
+          <Text></Text>
+          {/* Output */}
+        </View>
       </View>
-    </View>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
